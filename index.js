bf8f508f87c00ca9834c702c58cb481bbb015a48
FelipeTelesG <felipetelesg@gmail.com>
2022-05-18 19:09:05 -0400

    Microondas

---
+++ index.js
@@ -0,0 +1,41 @@
+/* CHECKPOINT PROGRAMACAO IMPERATIVA
+
+tentando fazer um microondas
+
+--------------------------------OPÇÕES--------------------------------
+1 - Pipoca
+2 - Macarrão
+3 - Carne 
+4 - Feijão
+5 - Brigadeiro
+
+*/
+
+function microondas (opcao, tempo) {
+    let tempoPadrao;
+    
+    if (opcao === 1) {
+        tempoPadrao = 10;
+    } else if (opcao === 2 || opcao === 5) {
+        tempoPadrao = 8;
+    } else if (opcao === 3) {
+        tempoPadrao = 15;  
+    } else if (opcao === 4) {
+        tempoPadrao = 12;   
+    } else {
+        return "Prato inexistente!"
+    }
+
+    if (tempo < tempoPadrao) {
+        return "Tempo insuficiente." + " Prato pronto, bom apetite!!!";
+    } else  if (tempo > (2 * tempoPadrao) && tempo <= (3 * tempoPadrao)) {
+        return "A comida queimou." + " Prato pronto, bom apetite!!!";
+    } else if (tempo > (3 * tempoPadrao)) {
+        return "Kabum!" + " Prato pronto, bom apetite!!!";
+    } else {
+        return " Prato pronto, bom apetite!!!"
+    }
+
+}
+
+console.log (microondas(2, 20));
